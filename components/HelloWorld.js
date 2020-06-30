4e5d033165fc0e3917055ccd8e35fc0abbc7c8e8
saqing <contact@saqing.net>
2020-06-30 03:52:38 +0000

    init

---
+++ components/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const HelloWorld = ({ name }) => {
+
+    return <>hello {name}</>
+}
+
+export default HelloWorld
---
+++ components/MyButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const MyButton = () => {
+
+    return <Button type="primary">My Button</Button>
+}
+
+export default MyButton
